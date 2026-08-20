e50a05ddeca35dc91ac1842a5f6cf1aff85469ed
Yoel Jimenez del Valle <yoel07@gmail.com>
2026-08-20 10:22:26 -0400

    add control url to start commanr

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,7 +74,8 @@ echo "Starting Single Stork Agent instance..."
   --host "${STORK_AGENT_HOST}" \
   --port "${STORK_AGENT_PORT}" \
   --server-url "${STORK_AGENT_SERVER_URL}" \
-  --prometheus-kea-exporter-address="${PROMETHEUS_EXPORTER_ADDR}" >> /var/log/kea/stork-agent.log 2> /var/log/kea/stork-agent.err &
+  --prometheus-kea-exporter-address="${PROMETHEUS_EXPORTER_ADDR}" \
+  --kea-control-agent-url "http://127.0.0.1:8000" >> /var/log/kea/stork-agent.log 2> /var/log/kea/stork-agent.err &
 
 # --- FOREGROUND PROCESS (Kea DHCPv4) ---
 echo "Starting Kea DHCPv4 Server..."
